441e9ba792ccc411caf2e1d5fc0ee9f95a4d1b88
arnavkartikeya <arnavkartikeya@gmail.com>
2020-01-01 21:45:08 -0800

    Includes user profile page with number of hours and upcoming events, but not personal events

---
+++ volunteir/ProfileCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCollectionViewCell.swift
+//  volunteir
+//
+//  Created by Arnav Kartikeya on 12/31/19.
+//  Copyright © 2019 Arnav Kartikeya. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ volunteir/UserProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserProfileViewController.swift
+//  volunteir
+//
+//  Created by Arnav Kartikeya on 12/31/19.
+//  Copyright © 2019 Arnav Kartikeya. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
